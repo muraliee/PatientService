c39cdd9d6f4194c3b61fd598ed049994ab865c6f
muraliee <muraliee.k@gmail.com>
2026-06-23 10:35:41 +0530

    terraform

---
+++ deployment/terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "patient-service"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "image_uri" {
+  description = "Container image URI to deploy (overrides ECR if provided)"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of CIDRs for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
